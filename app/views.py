b1f3e62e31d59f6148dfbb46ef385e9292eeb792
Nyambura🌻 <bianca@Biancas-MacBook-Air.local>
2022-05-04 05:19:42 +0300

    add article html and link it to views

--- app/views.py
+++ app/views.py
@@ -1,7 +1,7 @@
 
 from flask import render_template 
 from  app import app
-from .request import get_news ,get_news, get_category
+from .request import get_news ,get_news, get_category, article_source
 
 
 # Views
@@ -20,6 +20,15 @@ def index():
     
     return render_template('index.html', general = general_news, entertainment = entertainment_news,sports = sports_news)
 
+@app.route('/article/<id>')
+def article(id):
+
+    '''
+    View article page function that returns the various article details page and its data
+    '''
+    # title= 'Articles'
+    articles = article_source(id)
+    return render_template('article.html',articles= articles,id=id )
 
 @app.route('/categories/<categ_name>')
 def category(categ_name):
