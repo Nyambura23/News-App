b9e8292c391c1a1a4b800c6a5ef19e3ff9a8e1f7
Nyambura🌻 <bianca@Biancas-MacBook-Air.local>
2022-05-04 04:39:48 +0300

    add news html template

--- app/views.py
+++ app/views.py
@@ -1,7 +1,8 @@
 
 from flask import render_template 
 from  app import app
-from .request import get_news ,get_news
+from .request import get_news ,get_news, get_category
+
 
 # Views
 @app.route('/')
@@ -18,3 +19,15 @@ def index():
     # print(general_news)
     
     return render_template('index.html', general = general_news, entertainment = entertainment_news,sports = sports_news)
+
+
+@app.route('/categories/<categ_name>')
+def category(categ_name):
+    '''
+    function to return the categories.html page and its content
+    '''
+    category = get_category(categ_name)
+    title = f'{categ_name}'
+    categ = categ_name
+
+    return render_template('categories.html',title = title,category = category, categ= categ_name)
