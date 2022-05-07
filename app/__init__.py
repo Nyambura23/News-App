278a666060d31e1414c59a4f40d3845c58f5be5c
Nyambura🌻 <bianca@Biancas-MacBook-Air.local>
2022-05-07 23:05:13 +0300

    create a function that can be launched from the script

--- app/__init__.py
+++ app/__init__.py
@@ -1,20 +1,44 @@
+# from flask import Flask
+# from flask_bootstrap import Bootstrap
+# from config import config_options
+
+
+# bootstrap = Bootstrap()
+
+# def create_app(config_name):
+
+# # Initializing application
+#  app = Flask(__name__)
+
+# # Creating the app configurations
+#  app.config.from_object(config_options[config_name])
+
+# # Initializing Flask Extensions
+# bootstrap.init_app(app)
+
+
+# return app
+
+
 from flask import Flask
-from .config import DevConfig
 from flask_bootstrap import Bootstrap
+from config import config_options
 
+bootstrap = Bootstrap()
 
-# Initializing application
-app = Flask(__name__,instance_relative_config=True)
+def create_app(config_name):
 
-# Setting up configuration
-app.config.from_object(DevConfig)
-app.config.from_pyfile('config.py')
+    app = Flask(__name__)
 
-# Initializing Flask Extensions
-bootstrap = Bootstrap(app)
+    # Creating the app configurations
+    app.config.from_object(config_options[config_name])
 
-from app import views
-from app import error
+    # Initializing flask extensions
+    bootstrap.init_app(app)
+
+    # Will add the views and forms
+
+    return app
 
 
 
--- app/config.py
+++ app/config.py
@@ -12,25 +12,11 @@ class Config:
    
 
 
-
 class ProdConfig(Config):
-    '''
-    Production  configuration child class
-
-    Args:
-        Config: The parent configuration class with General configuration settings
-    '''
     pass
 
 
 class DevConfig(Config):
-    '''
-    Development  configuration child class
-
-    Args:
-        Config: The parent configuration class with General configuration settings
-    '''
-
     DEBUG = True
 
 config_options = {
